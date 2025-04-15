409bc36ae71a00e1328859bbee6682b1607059b6
Nikolay Rodionov <rodionov.nikolay@gmail.com>
2025-04-15 21:43:39 +0200

    Initial Commit

---
+++ SpaceXLaunches/SpaceXLaunchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXLaunchesApp.swift
+//  SpaceXLaunches
+//
+//  Created by Nikolai Rodionov on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXLaunchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
